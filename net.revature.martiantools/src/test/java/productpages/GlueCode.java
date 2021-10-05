10573433975c42ec344fa4116a5a22c61d034398
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2021-10-04 21:39:40 -0400

    add Products pages separated by category cucumber.

---
+++ net.revature.martiantools/src/test/java/productpages/GlueCode.java
@@ -0,0 +1,32 @@
+package productsPages;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+// below code is temporary placeholder
+
+public class GlueCode {
+	@When("the user does this one <data>")
+	public void the_user_does_this_one() {
+			// Write code here that turns the phrase above into concrete actions
+			throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user does this two <data>")
+	public void tthe_user_does_this_two() {
+			// Write code here that turns the phrase above into concrete actions
+			throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user does this three <data>")
+	public void the_user_does_this_three() {
+			// Write code here that turns the phrase above into concrete actions
+			throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user does this four <data>")
+	public void the_user_does_this_four() {
+			// Write code here that turns the phrase above into concrete actions
+			throw new io.cucumber.java.PendingException();
+	}
+}
---
+++ net.revature.martiantools/src/test/java/productpages/productsPages.feature
@@ -0,0 +1,21 @@
+#Author: ars0148@gmail.com
+#Keywords Summary :
+#Feature: List of scenarios.
+#Scenario: Business rule through list of steps with arguments.
+#Given: Some precondition step
+#When: Some key actions
+#Then: To observe outcomes or validation
+#And,But: To enumerate more Given,When,Then steps
+#Scenario Outline: List of steps for data-driven as an Examples and <placeholder>
+#Examples: Container for s table
+#Background: List of steps run before each of the scenarios
+#""" (Doc Strings)
+#| (Data Tables)
+#@ (Tags/Labels):To group Scenarios
+#<> (placeholder)
+#""
+## (Comments)
+#Sample Feature Definition Template
+Feature: Products pages for MartianTools
+
+# Temporary placeholder here for Feature
