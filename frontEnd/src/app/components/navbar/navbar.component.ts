f915ce73112d04f4670d166164dbe3cd5384fb98
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2021-10-15 10:57:21 -0400

    Possible navBar right now for test.

--- frontEnd/src/app/components/navbar/navbar.component.ts
+++ frontEnd/src/app/components/navbar/navbar.component.ts
@@ -1,34 +1,42 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, ElementRef } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { Login } from 'src/app/service/login';
 import { LoginService } from 'src/app/service/login.service';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+	selector: 'app-navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit, OnDestroy {
 
-  isLoggedin = false;
-  private userSub: Subscription = new Subscription;
-  constructor(private loginServ:LoginService) {
-   }
+	isLoggedin = false;
+	private userSub: Subscription = new Subscription;
+	constructor(private loginServ:LoginService, private elementRef:ElementRef) {
+	 }
 
+	ngOnDestroy(): void {
+	 this.userSub.unsubscribe();
+	}
 
+	fname:string="";
 
-  ngOnDestroy(): void {
-   this.userSub.unsubscribe();
-  }
+	ngOnInit() {
+		this.userSub = this.loginServ.user.subscribe(user=>{
+			this.isLoggedin =!user ? false : true; 
+			this.fname=user.user_fname;
+		});
+	}
 
-fname:string="";
-  ngOnInit() {
-   this.userSub = this.loginServ.user.subscribe(user=>{
-    this.isLoggedin =!user ? false : true; 
-    this.fname=user.user_fname;
-   });
-  }
-  onlogout(){
-    this.loginServ.logout();
-  }
+	ngAfterViewInit() {
+		const navbarToggler = document.getElementsByClassName("navToggler")[0];
+		let link = document.getElementsByClassName("navLink")[0];
+		navbarToggler.addEventListener('click', () => {
+			link.classList.toggle('active');
+		});
+	}
+
+	onlogout(){
+		this.loginServ.logout();
+	}
 }
