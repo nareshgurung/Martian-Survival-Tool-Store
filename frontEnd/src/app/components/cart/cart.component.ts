dddd11673eeb6b70b25b5b04f84fb01131dbbf76
cferraz <43863678+carolinaferraz@users.noreply.github.com>
2021-10-17 02:12:03 -0700

    refactored cart, new form and success msg

--- frontEnd/src/app/components/cart/cart.component.ts
+++ frontEnd/src/app/components/cart/cart.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { CartService } from 'src/app/service/cart.service';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-cart',
@@ -8,23 +9,38 @@ import { CartService } from 'src/app/service/cart.service';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+@Input() product? : Product;
 
+items = this.cartService.getItems();
 
-  items = this.cartService.getItems();
-
-  checkoutForm = this.formBuilder.group({
-    name: '',
-    address: ''
-  });
 
   constructor(
     private cartService: CartService,
     private formBuilder: FormBuilder,
   ) {}
 
+  checkoutForm = this.formBuilder.group({
+    email: '',
+    name: '',
+    address: '',
+    zip: '',
+    city: '',
+    state: '',
+
+    card:'',
+    exp: '',
+    code: '',
+  });
+
+
+  confirmationMsg:boolean = false;
+
+  showMessage() {
+    this.confirmationMsg = !this.confirmationMsg;
+  }
+
   onSubmit(): void {
     this.items = this.cartService.clearCart();
-    console.warn('Your order has been submitted', this.checkoutForm.value);
     this.checkoutForm.reset();
   }
 }
